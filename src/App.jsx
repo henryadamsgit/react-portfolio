0d54e3039296418b5bee611c6fbbe0368880a8e6
Henry Adams <henrybruce.adams@icloud.com>
2023-05-23 22:01:34 +0100

    Added boilerplate to files

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import "./App.scss";
 function App() {
   return (
     <div className="App">
-      <h1>Hi, welcome to my portfolio!</h1>
     </div>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <div className="header"></div>
+      <section className="header__background">
+        <h1>Henry Adams</h1>
+        <h3>Junior Software Developer</h3>
+      </section>
+    </>
+  );
+};
+
+export default Header;
